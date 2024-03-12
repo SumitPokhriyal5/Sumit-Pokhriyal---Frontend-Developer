d48d1d7b87012482438ef366d8d8c3e95f4b93af
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-12 13:58:24 +0530

    created banner for the home page

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+
+function Banner() {
+  return (
+    <div className="flex justify-center items-center bg-banner-image min-h-[650px] bg-center bg-cover bg-no-repeat p-[6rem_5%] max-[600px]:p-[6rem_10px]">
+      <p className="max-[700px]:p-[5rem] max-[700px]:mt-[60px] max-[700px]:bg-[rgba(0,0,0,0.3)] rounded-[20px] font-shojumaru text-[2.5rem] text-white text-center leading-[119%] max-[550px]:text-[1.8rem]">
+        Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
