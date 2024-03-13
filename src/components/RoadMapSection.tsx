19afd7f7771f2a255c66d179e013aaa5c9317276
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-13 17:11:15 +0530

    improved font size and spacing of road map section

--- src/components/RoadMapSection.tsx
+++ src/components/RoadMapSection.tsx
@@ -10,7 +10,7 @@ const RoadMapSection = () => {
       <div className="bg-black w-[100%] flex flex-col items-center text-white">
         <div className="bg-roadmap-bg bg-no-repeat w-[100%] flex flex-col items-center p-[6rem_5%] max-[600px]:p-[6rem_10px]">
           <div className="flex flex-col items-center w-[100%] gap-[5rem]">
-            <h3 className="text-samurai-red font-shojumaru text-[3rem]">
+            <h3 className="text-samurai-red font-shojumaru text-[2.5rem]">
               Road Map
             </h3>
             <div className="flex items-center justify-center flex-wrap gap-[3rem] max-[1200px]:gap-[5rem] w-[100%] h-[100%]">
