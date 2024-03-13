b06aec6758c5bba7077fe898c936eef05030df82
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-13 17:08:28 +0530

    improved font size and spacing of takenomics section

--- src/components/TokenomicSection.tsx
+++ src/components/TokenomicSection.tsx
@@ -48,41 +48,41 @@ function TokenomicSection() {
       <div className="bg-black w-[100%] h-fit flex flex-col items-center text-white">
         <div className="bg-tokenomics-bg bg-no-repeat w-[100%] flex flex-col items-center p-[6rem_5%] max-[600px]:p-[6rem_10px]">
           <div className="flex flex-col items-center w-[100%] gap-[1rem] max-[600px]:gap-[2rem]">
-            <h3 className="text-samurai-red font-shojumaru text-[3rem]">
+            <h3 className="text-samurai-red font-shojumaru text-[2.5rem]">
               Tokenomics
             </h3>
-            <div className="flex items-start justify-center max-[800px]:items-center max-[800px]:flex-col gap-[3rem] w-[100%] h-[100%]">
+            <div className="flex items-center justify-center max-[800px]:items-center max-[800px]:flex-col gap-[3rem] w-[100%] h-[100%]">
               <div ref={leftDivRef} className="flex flex-col items-center w-[40%] max-[800px]:w-full">
                 <div className="flex flex-col items-center justify-center ">
-                  <div className="bg-[#ED0137] flex justify-center items-center translate-y-[2rem] rounded-[2rem] p-4 ">
+                  <div className="bg-[#ED0137] flex justify-center items-center translate-y-[1.6rem] rounded-[1rem] p-[1rem_2rem] ">
                     <p className="font-shojumaru text-[1rem] text-white">
                       Token Info
                     </p>
                   </div>
-                  <div className="w-full flex flex-col justify-center items-center gap-[1.2rem] border-[2px] border-[#EF490F] rounded-[20px] p-[2rem] pb-[1rem]">
+                  <div className="w-[full] flex flex-col justify-center items-center gap-[1.2rem] border-[2px] border-[#EF490F] rounded-[20px] p-[2rem] pb-[1rem]">
                     <table className="w-[100%]">
-                      <tbody className="font-zcool text-[1.5rem] flex flex-col gap-[1.2rem] w-[100%]">
-                        <tr className="w-[100%]">
-                          <td className="w-[100%]">Name</td>
-                          <td className="text-[#DF180A] w-[100%] text-left">
+                      <tbody className="font-zcool text-[1.2rem] flex flex-col gap-[1.2rem] w-[100%]">
+                        <tr className="flex justify-between gap-10">
+                          <td className="">Name</td>
+                          <td className="text-[#DF180A] text-left">
                             Saviour
                           </td>
                         </tr>
-                        <tr className="w-[100%]">
-                          <td className="w-[100%]">Symbol</td>
-                          <td className="text-[#DF180A] text-left w-[100%]">
+                        <tr className="flex justify-between gap-10">
+                          <td className="">Symbol</td>
+                          <td className="text-[#DF180A] text-left ">
                             SVR
                           </td>
                         </tr>
-                        <tr className="w-[100%]">
-                          <td className="w-[100%]">Total Supply</td>
-                          <td className="text-[#DF180A] text-left w-[100%]">
+                        <tr className="flex justify-between gap-10">
+                          <td className="">Total Supply</td>
+                          <td className="text-[#DF180A] text-left ">
                             1000 Trillion
                           </td>
                         </tr>
-                        <tr className="w-[100%]">
-                          <td className="w-[100%]">Decimals</td>
-                          <td className="text-[#DF180A] text-left w-[100%]">
+                        <tr className="flex justify-between gap-10">
+                          <td className="">Decimals</td>
+                          <td className="text-[#DF180A] text-left ">
                             18
                           </td>
                         </tr>
