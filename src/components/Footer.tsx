a454d1f8a19b1cd26d445e6d5dc8136977eed9e9
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-13 17:28:20 +0530

    improved font size and spacing of footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-[3.6rem] p-[4rem] max-[600px]:p-[5rem_13px] bg-gradient-to-t from-[#120000] to-[#020505]">
+    <div className="flex flex-col items-center justify-center gap-[3rem] p-[4rem] max-[600px]:p-[5rem_13px] bg-gradient-to-t from-[#120000] to-[#020505]">
     <img
       width={200}
       height={163}
@@ -15,10 +15,10 @@ const Footer = () => {
         color: "transparent",
         backgroundImage: "linear-gradient(to right, #FE012A, #760107)",
       }}
-      className="font-inter text-[2.4rem] font-bold">
+      className="font-inter text-[2.5rem] font-bold">
       SITEMAP
     </p>
-    <div className="flex gap-[5rem] items-center justify-center text-white text-[12.82px] font-inter font-bold flex-wrap leading-[0.1rem]">
+    <div className="flex gap-[2rem] items-center justify-center text-white text-[1rem] font-inter font-bold flex-wrap leading-[0.1rem]">
       <p className="cursor-pointer">Home</p>
       <p className="cursor-pointer">IDO</p>
       <p className="cursor-pointer">Tokenomics</p>
